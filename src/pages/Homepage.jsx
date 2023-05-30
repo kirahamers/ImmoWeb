cd19565b9a9f383131f0c67fe4bf2b52ce2811b2
kira <104847182+shakirahamers@users.noreply.github.com>
2023-05-30 22:27:41 +0200

    home en detailpage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import Filter from '../components/Filter';
 import { add } from '../store/favorites/slice';
 import { useDispatch } from 'react-redux';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import axios from "axios";
 
 const huizenData = require('../utils/huizen.json');
 
@@ -32,29 +33,84 @@ const Homepage = () => {
     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
+  const [panden, setPanden] = useState([]);
+  const [typePanden, setTypePanden] = useState({});
+  const [afbeeldingen, setAfbeeldingen] = useState({});
+
+  useEffect(() => {
+    fetchPanden();
+    fetchTypePanden();
+    fetchAfbeeldingen();
+  }, []);
+
+  const fetchPanden = async () => {
+    try {
+      const response = await axios.get("/panden");
+      setPanden(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchTypePanden = async () => {
+    try {
+      const response = await axios.get("/typepanden");
+      const types = response.data.reduce((acc, type) => {
+        acc[type.id] = type.naam;
+        return acc;
+      }, {});
+      setTypePanden(types);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchAfbeeldingen = async () => {
+    try {
+      const response = await axios.get("/afbeeldingen");
+      const images = response.data.reduce((acc, image) => {
+        const pandId = image.pandId;
+        if (!acc[pandId]) {
+          acc[pandId] = [];
+        }
+        acc[pandId].push(image.url);
+        return acc;
+      }, {});
+      setAfbeeldingen(images);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Filter></Filter>
       <div className="p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center gap-4">
-          {huizenData.panden.map((h) => (
+          {panden.map((pand) => (
             <div
-              onClick={() => navigate(`/huizen/${h.id}`)}
-              key={h.id}
+              onClick={() => navigate(`/huizen/${pand.id}`)}
+              key={pand.id}
               className="relative w-full h-auto flex flex-col justify-between overflow-clip align-middle rounded-md shadow-md cursor-pointer"
             >
-              <img
-                className="w-full h-3/4 object-cover"
-                src={h.afbeelding}
-                alt={h.beschrijving}
-              />
-              <p className="font-semibold text-center">{h.beschrijving}</p>
-              <p className="text-center -mt-3 text-gray-400">{h.gemeente}, €{h.prijs}</p>
+              {afbeeldingen[pand.id] && (
+                <img
+                  className="w-full h-3/4 object-cover"
+                  src={afbeeldingen[pand.id][0]} // Gebruik de eerste afbeelding van het pand
+                  alt="Pand afbeelding"
+                />
+              )}
+              <p className="font-semibold text-center">{pand.beschrijving}</p>
+              <p className="text-center -mt-3 text-gray-400">{pand.gemeente}, €{pand.prijs}</p>
               <button
-                onClick={(event) => handleHeartClick(event, h)}
+                onClick={(event) => handleHeartClick(event, pand)}
                 className="absolute bottom-2 right-2"
               >
+                {favorites.includes(pand.id) ? (
+                  <AiFillHeart color="red" size={22} />
+                ) : (
                   <AiOutlineHeart color="red" size={22} />
+                )}
               </button>
             </div>
           ))}
@@ -62,6 +118,6 @@ const Homepage = () => {
       </div>
     </>
   );
-};
+                }  
 
 export default Homepage;
